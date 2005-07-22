59e345f5888653a29fef233b97ce285d78fc3446
Luis Pedro Coelho <luis@luispedro.org>
2005-07-22 23:43:25 +0000

    Get more info

--- scripts/do-test.zsh
+++ scripts/do-test.zsh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+max=10000
+step=100
 function do_test() {
 	[[ -d output ]] || mkdir output
 	input=textes/$1
@@ -7,15 +9,18 @@ function do_test() {
 	toutput=tmp/output
 	output=output/$1.results
 	[[ -f $output ]] && rm $output
-	for ((i=100;i<10001;i=i+100)); do
+	for (( i=$step; i<$max+1; i=i+$step )); do
 		echo -n .
 		./textes/get-bytes $i < $input >$tinput
 		./src/suffixtree f $tinput CACC >$toutput
 		string_size=`grep 'String size:' $toutput | sed 's,[^0-9],,g'`
 		nodes_without=`grep 'Nodes without' $toutput| sed 's,[^0-9],,g'`
 		nodes_with=`grep 'Nodes with ' $toutput| sed 's,[^0-9],,g'`
-		echo $string_size $nodes_without $nodes_with >>$output
+		time_full=`grep 'timer\[full' $toutput | sed 's,[^0-9],,g'`
+		time_add=`grep 'timer\[add' $toutput | sed 's,[^0-9],,g'`
+		echo $string_size $nodes_without $nodes_with $time_full $time_add >>$output
 	done
+	echo
 	echo
 	rm $tinput
 	rm $toutput
