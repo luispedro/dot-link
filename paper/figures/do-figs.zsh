0b616dc3bc36f90c9f7b56343d511c1a6ec92b20
Luis Pedro Coelho <luis@luispedro.org>
2005-08-11 15:14:57 +0000

    Small fixes. Longer, more detailed explanations.

--- paper/figures/do-figs.zsh
+++ paper/figures/do-figs.zsh
@@ -1,11 +1,6 @@
 #!/bin/zsh
 format=eps
-fig=(aaaa.fig merge.fig mississipi-0.fig)
-
+figs=(aaaa.fig merge.fig mississipi-0.fig mississipi-nodes mississipi-1)
 transfig -L $format $figs
-transfig -L $format -m .5 mississipi-nodes
-transfig -L $format -m .25 mississipi-1
-
-
 make
 
