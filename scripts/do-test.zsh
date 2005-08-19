78fd855c376b7f7b8d50d02e301cbced8ba6695a
Luis Pedro Coelho <luis@luispedro.org>
2005-08-19 13:48:25 +0000

    More descriptive output file name New commandline interface for driver

--- scripts/do-test.zsh
+++ scripts/do-test.zsh
@@ -1,18 +1,19 @@
 #!/bin/zsh
 
-max=10000
+max=100000
 step=100
+k=1
 function do_test() {
 	[[ -d output ]] || mkdir output
 	input=textes/$1
 	tinput=tmp/input
 	toutput=tmp/output
-	output=output/$1.results
+	output=output/"machine=`hostname`.in=$1.k=$k.results"
 	[[ -f $output ]] && rm $output
 	for (( i=$step; i<$max+1; i=i+$step )); do
 		echo -n .
 		./textes/get-bytes $i < $input >$tinput
-		./src/suffixtree f $tinput CACC >$toutput
+		./src/suffixtree $k $tinput CACC >$toutput
 		string_size=`grep 'String size:' $toutput | sed 's,[^0-9],,g'`
 		nodes_without=`grep 'Nodes without' $toutput| sed 's,[^0-9],,g'`
 		nodes_with=`grep 'Nodes with ' $toutput| sed 's,[^0-9],,g'`
