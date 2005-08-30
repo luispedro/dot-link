7bf7bd7b3a26a5215e95888e493470062f4e3cba
Luis Pedro Coelho <luis@luispedro.org>
2005-08-30 15:25:16 +0000

    Allow calling in succession Generate ratio plots

--- scripts/do-plots.zsh
+++ scripts/do-plots.zsh
@@ -2,8 +2,8 @@
 
 function plot() {
 	output="$1.plot.png"
-	input="$1.results"
-	/usr/local/bin/gnuplot <<-END
+	input="$1"
+	gnuplot <<-END
 		set terminal png color
 		set title 'Number of nodes vs Text Size'
 		set xlabel 'Number of Characters'
@@ -12,10 +12,22 @@ function plot() {
 
 		set output "$output"
 		plot '$input' using 1:2 with lines, '$input' using 1:3 with lines
+		set output "ratio-$output"
+		plot  '$input' using 1:(\$3/\$2) with lines
 	END
 }
 
-cd ../output || (echo "Error: no output directory" &1>2 ; exit 1)
-plot dna
-plot english
-plot random
+if test -d ../output; then
+	d=../output
+elif test -d ./output; then
+	d=./output
+else
+	echo "Error: no output directory" &1>2
+	exit 1
+fi
+
+cd $d
+
+for f in *.results; do
+	plot $f
+done
