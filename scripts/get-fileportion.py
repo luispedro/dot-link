87a4fa08846926d0d749b35b8f60a8f00bba2e49
Luis Pedro Coelho <luis@luispedro.org>
2005-09-17 12:36:21 +0000

    Make the python script do the repeats itself

--- scripts/get-fileportion.py
+++ scripts/get-fileportion.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from sys import argv
+import sys
 import random
 import os
 
@@ -9,23 +10,30 @@ filelen=os.stat(inputfile)[6]
 lenght=int(argv[3])
 k=int(argv[4])
 
-if argv[2] == '.':
-	begin = random.randrange(filelen-lenght)
-else:
-	begin=int(argv[2])
-	if begin + lenght > filelen:
-		raise Exception('File not large enough')
-
 f=open(inputfile,'r')
-f.seek(begin)
-string = f.read(lenght)
+repeats=1
+if len(argv) > 5:
+	repeats=int(argv[5])
 
-changed = []
-for point in random.sample(range(0,len(string)),k):
-	# '#' is a character which does not show up in any of our data files
-	if point > 0:
-		string = string[0:point-1] + '#' + string[point:]
+def get_string(f,begin,lenght,k):
+	f.seek(begin)
+	string = f.read(lenght)
+	for point in random.sample(range(0,len(string)),k):
+		# '#' is a character which does not show up in any of our data files
+		if point > 0:
+			string = string[0:point-1] + '#' + string[point:]
+		else:
+			string = '#' + string[1:]
+
+	print string
+
+
+for i in range(repeats):
+	if argv[2] == '.':
+		begin = random.randrange(filelen-lenght)
 	else:
-		string = '#' + string[1:]
+		begin=int(argv[2])
+		if begin + lenght > filelen:
+			raise Exception('File not large enough')
+	get_string(f,begin,lenght,k)
 
-print string
