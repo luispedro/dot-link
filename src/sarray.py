f3598c97e9b945970a4f8537989abf781405b149
Luis Pedro Coelho <luis@luispedro.org>
2007-05-21 21:59:00 +0000

    Work with inputs not called 'file'
    Correct for '\0' dollar

--- src/sarray.py
+++ src/sarray.py
@@ -1,6 +1,12 @@
-input=file('file')
+import sys
 
-string=input.readline().strip() + '$'
+filename='file'
+if len(sys.argv) >= 1:
+    filename=sys.argv[1]
+
+input=file(filename)
+
+string=input.readline().strip()
 
 naive=range(len(string))
 def sufcmp(a,b):
@@ -10,17 +16,18 @@ def sufcmp(a,b):
 		a += 1
 		b += 1
 	if a == len(string):
-		return 1
-	if b == len(string):
 		return -1
+	if b == len(string):
+		return +1
 	if string[a] < string[b]:
 		return -1
 	return 1
 naive.sort(sufcmp)
+naive.insert(0,len(string)) # For the '\0' !
 	
 from os import popen
 import re
-input=popen('./suffixtree 1 file issi')
+input=popen('./suffixtree %s' % filename)
 num = re.compile('^\d+$')
 stree=[]
 for line in input: 
@@ -31,3 +38,5 @@ if naive != stree:
 	print "Error"
 else:
 	print "OK"
+
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
